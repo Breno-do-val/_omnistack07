5581daa6369a28b3fd7e577121b62ae4772f6c53
breno-do-val <brenorvale@gmail.com>
2020-02-23 21:11:51 -0300

    Backend concluded

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,11 +1,42 @@
-const Post = require('../models/Post')
+const Post = require('../model/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     async index(req, res) {
+        const posts = await Post.find().sort('-createdAt')
 
+        res.json(posts)
     },
 
     async store(req, res) {
-        return res.json({ ok: true })
+        const { author, place, description, hashtags } = req.body
+        const { filename: image } = req.file
+
+        //Script for resizing and saving the image as JPG
+        const [name] = image.split('.')
+        const fileName = `${name}.jpg`
+        
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70})
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName)
+            )
+        //Command to delelte the original image
+        fs.unlinkSync(req.file.path)
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image: fileName,
+        })
+
+        req.io.emit('post', post)
+
+        return res.json(post)
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
-mongoose.connect('mongodb+srv://breno:Br3n0@cluster0-fxdie.mongodb.net/test?retryWrites=true&w=majority', {
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+mongoose.connect('mongodb+srv://breno:Br3n0@cluster0-fxdie.mongodb.net/test?retryWrites=true&w=majority', { 
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+
+app.use((req, res, next) => {
+    req.io = io
+
+    next()
+})
+
+app.use(cors())
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'))
 
-app.listen(3000)
-
-//stop at 30min - configuring Post method
+Server.listen(3000)
